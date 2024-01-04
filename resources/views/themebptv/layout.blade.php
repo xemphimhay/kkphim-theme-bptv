afdd476e224428e4dcb10e741a2f56c390f17259
xemphimhay <lanhp871@gmail.com>
2024-01-04 19:31:35 +0700

    ok

--- resources/views/themebptv/layout.blade.php
+++ resources/views/themebptv/layout.blade.php
@@ -36,6 +36,7 @@
 @endphp
 
 @push('header')
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
     <link href="https://fonts.googleapis.com/css?family=Source+Sans+Pro" rel="stylesheet">
     <link href="https://fonts.googleapis.com/css?family=Encode+Sans" rel="stylesheet">
     <link href="https://fonts.googleapis.com/css?family=Merriweather+Sans" rel="stylesheet">
